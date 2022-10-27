db139575b00a52f938ff6513efcef7f355d91454
ShorunTech <gshoronke@gmail.com>
2022-10-27 13:37:48 +0100

    Update goalController.js
    updated

--- Backend/controllers/goalController.js
+++ Backend/controllers/goalController.js
@@ -5,7 +5,7 @@ const Goal = require('../models/goalModel')
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = asyncHandler(async (req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
     const goals = await Goal.find()
 
     res.status(200).json(goals)
@@ -14,12 +14,12 @@ const getGoals = asyncHandler(async (req, res) => {
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
-const setGoal = asyncHandler(async (req, res) => {
+const setGoal = asyncHandler(async(req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    
+
     const goal = await Goal.create({
         text: req.body.text,
     })
@@ -30,15 +30,37 @@ const setGoal = asyncHandler(async (req, res) => {
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
+const updateGoal = asyncHandler(async(req, res) => {
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+
+    res.status(200).json(updatedGoal)
 })
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+const deleteGoal = asyncHandler(async(req, res) => {
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 
@@ -48,5 +70,3 @@ module.exports = {
     updateGoal,
     deleteGoal,
 }
-
-
