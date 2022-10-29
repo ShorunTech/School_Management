639dfa250ae8ee46081895dfcb62bcd34571f5ee
ShorunTech <gshoronke@gmail.com>
2022-10-29 08:02:46 +0100

    userController
    update jwt and bcrypt.

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,27 +1,32 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async(req, res) => {
     res.json({ message: 'Register User' })
-}
+})
 
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async(req, res) => {
     res.json({ message: 'login User' })
-}
+})
 
 // @desc    Get user data
 // @route   POST /api/users/me
 // @access  Public
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async(req, res) => {
     res.json({ message: 'User data display' })
-}
+})
 
 module.exports = {
     registerUser,
