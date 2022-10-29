c159bf611a15665495d572842fdca217c69c91ae
ShorunTech <gshoronke@gmail.com>
2022-10-29 10:30:05 +0100

    userController

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -33,7 +33,22 @@ const registerUser = asyncHandler(async(req, res) => {
         name,
         email,
         password: hashedPassword,
-      })
+    })
+
+    if (user) {
+        res.status(201).json({
+          _id: user.id,
+          name: user.name,
+          email: user.email,
+          
+        })
+      } else {
+        res.status(400)
+        throw new Error('INvalid user data')
+      }
+  
+
+
 
 
     res.json({ message: 'Register User' })
