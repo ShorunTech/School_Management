9f4e1e23e6ac348f9d111a458621025590e5cf44
ShorunTech <gshoronke@gmail.com>
2022-11-02 09:23:12 +0100

    authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,3 +1,27 @@
 const jwt = require('jsonwebtoken')
 const asyncHandler = require('express=async-handler')
 const User = require ('../models/userModel')
+
+
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token
+
+
+    if(req.headers.authorization && 
+        req.headers.autorization.startsWith('Bearer')
+        ){
+            try {
+                //Get token fron header
+                token = req.headers.authorization.split('')[1]
+            }
+        }
+
+
+
+
+
+
+
+})
+
