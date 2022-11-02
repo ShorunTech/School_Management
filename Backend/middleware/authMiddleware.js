0fb30d3b9dfcd8dabee932354cac7523001ce3f0
ShorunTech <gshoronke@gmail.com>
2022-11-02 10:08:28 +0100

    authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -14,6 +14,14 @@ const protect = asyncHandler(async (req, res, next) => {
             try {
                 //Get token fron header
                 token = req.headers.authorization.split('')[1]
+
+                // Verify token
+                const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+                // Get user from the token
+                req.user = await User.findById(decoded.id).select('-password')
+
+              next()
             }
         }
 
