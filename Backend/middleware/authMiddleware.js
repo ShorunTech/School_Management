55ca3245098b6a0129a66ed1a8e2998608129f68
ShorunTech <gshoronke@gmail.com>
2022-11-02 10:15:15 +0100

    authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -29,11 +29,12 @@ const protect = asyncHandler(async (req, res, next) => {
             }
         }
 
+        if (!token) {
+            res.status(401)
+            throw new Error('Not authorized, no token')
+          }
 
-
-
-
-
-
-})
+    }
+    
+)
 
