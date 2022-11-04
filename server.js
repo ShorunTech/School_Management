9e70b6170135ba1d821818914760597922aba4e2
ShorunTech <gshoronke@gmail.com>
2022-11-04 11:06:20 +0100

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const colors = require('colors')
-const dotenv =  require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
@@ -10,9 +10,9 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false }))
+app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/goals', require('./Backend/routes/goalRoutes'))
 
 app.use(errorHandler)
 
