2c3a15cc0db04f6123d088f1e0fd772ac23a895a
ShorunTech <gshoronke@gmail.com>
2022-11-07 19:48:53 +0100

    userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,17 +74,3 @@ const loginUser = asyncHandler(async(req, res) => {
     }
 
 })
-
-// @desc    Get user data
-// @route   POST /api/users/me
-// @access  Public
-
-const getMe = asyncHandler(async(req, res) => {
-    res.json({ message: 'User data display' })
-})
-
-module.exports = {
-    registerUser,
-    loginUser,
-    getMe,
-}
